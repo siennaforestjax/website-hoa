6bd575590dabdbae77c6d964da5c9b567f56ec64
Aaron <the_flicker7@hotmail.com>
2022-06-10 20:04:23 -0400

    fixing css

---
+++ website-hoa/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="nav-container">
+      <ul className="nav-list">
+        <li>Events</li>
+        <li>About</li>
+        <li>Docs</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
