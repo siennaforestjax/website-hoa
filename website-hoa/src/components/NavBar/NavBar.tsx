a7b3d8a47a68a381c17626fb752aa922c4424399
Aaron <the_flicker7@hotmail.com>
2022-06-14 16:08:23 -0400

    basic slide-out of nav panel

--- website-hoa/src/components/NavBar/NavBar.tsx
+++ website-hoa/src/components/NavBar/NavBar.tsx
@@ -2,15 +2,35 @@ import './NavBar.css';
 
 const NavBar = ({ ...isLoggedIn }) => {
   function displayContactList() {
-    return <li>Contact List (Log in)</li>;
+    return isLoggedIn ? (
+      <li>
+        <a>Contact List</a>
+      </li>
+    ) : (
+      <li>Contact List (Log in)</li>
+    );
+  }
+
+  function toggleNavPanel() {
+    document.getElementsByClassName('nav-panel')[0].classList.toggle('open');
   }
 
   return (
     <nav className="nav-container">
-      <button className="nav-hamburger">l l l</button>
+      <button className="nav-hamburger" onClick={toggleNavPanel}>
+        l l l
+      </button>
       <div className="nav-panel">
+        <button type="button" className="exit-button" onClick={toggleNavPanel}>
+          X
+        </button>
         <ul className="nav-list">
-          <li>Events</li>
+          <li>
+            Events
+            <ul className="nav-sub-list">
+              <li>See All</li>
+            </ul>
+          </li>
           <li>
             Docs
             <ul className="nav-sub-list">
