07eebeae7ff19ba9fefbd849413bb980c84b0851
fl1cker <the_flicker7@hotmail.com>
2022-06-25 10:06:01 -0400

    boardPage component test

--- website-hoa/src/components/BoardPage/BoardPage.spec.tsx
+++ website-hoa/src/components/BoardPage/BoardPage.spec.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import BoardPage from './BoardPage';
 import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
 import testData from '../../boardMembers.json';
 
 describe('The Board Page', () => {
-  it('renders as expected', () => {
-    const tree = renderer
-      .create(<BoardPage boardMembers={testData.boardMembers} />)
-      .toJSON();
+  
+  describe('The Snapshot', () => {
 
-    expect(tree).toMatchSnapshot();
+    it('renders as expected', () => {
+      const tree = renderer
+        .create(<BoardPage boardMembers={testData.boardMembers} />)
+        .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('The component', () => {
+    it('maps divs based on props', () => {
+      const tree = renderer.create(<BoardPage boardMembers={testData.boardMembers} />);
+      const component = tree.root;
+      
+      const sections = component.findAllByType('section');
+
+      expect(sections.length).toBe(testData.boardMembers.length);
+  
+    });
   });
 });
+
+
