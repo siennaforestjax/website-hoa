670c84ce2849b413c4d716b6b2ae0679a7eb8aa5
fl1cker <the_flicker7@hotmail.com>
2022-09-05 11:53:53 -0400

    scalable imageWithLoadingFallback

--- express-static-serve/website/src/components/ImageWithLoadingFallback/ImageWithLoadingFallback.tsx
+++ express-static-serve/website/src/components/ImageWithLoadingFallback/ImageWithLoadingFallback.tsx
@@ -5,9 +5,10 @@ import classes from './ImageWithLoadingFallback.module.css';
 type ImageWithLoadingFallbackProps = {
     source: string;
     altText: string;
+    loadingScale?: number;
 }
 
-function ImageWithLoadingFallback({source, altText, ...props} : ImageWithLoadingFallbackProps) {
+function ImageWithLoadingFallback({source, altText, loadingScale = 1, ...props} : ImageWithLoadingFallbackProps) {
 
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
@@ -24,7 +25,7 @@ function ImageWithLoadingFallback({source, altText, ...props} : ImageWithLoading
     return (
         <div className={`${classes.imageWrapper} ${isLoading ? classes.loading : isError ? classes.error : ''}`}> 
         {
-            isLoading ? <LoadingSvg /> : isError ? <p className={classes.errorMessage}>Image Failed To Load</p> : ''
+            isLoading ? <LoadingSvg scale={loadingScale}/> : isError ? <p className={classes.errorMessage}>Image Failed To Load</p> : ''
         }
             <img className={classes.mainImage}src={source} alt={altText} onLoad={onLoadHandler} onError={onErrorHandler} {...props}/>
         </div>
