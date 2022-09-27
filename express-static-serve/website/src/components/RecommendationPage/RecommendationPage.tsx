1f7d44aa720af701aa9c19650f928f56bfb7d89e
fl1cker <the_flicker7@hotmail.com>
2022-09-26 21:55:07 -0400

    color tweaks

--- express-static-serve/website/src/components/RecommendationPage/RecommendationPage.tsx
+++ express-static-serve/website/src/components/RecommendationPage/RecommendationPage.tsx
@@ -8,21 +8,22 @@ import mock from '../../recommendations.json';
 function RecommendationPage() {
   
   return (
-    <>
+    <div className={classes.pageWrapper}>
       <h1>Recommendations</h1>
-      <p>
-        The following page consists of recommendations provided by your fellow
-        neighbors
+      <p className={classes.pageDescription}>
+        Check out these recommendations provided by your fellow
+        neighbors.
       </p>
-      <div className={classes.recommendationContainer}>
+      <div className={classes.recommendationListContainer}>
         {mock.recommendations
           .filter((x) => (x.category === 'contractor'))
-          .slice(0, 1)
           .map((x) => (
-            <Recommendation key={x.id} {...x} />
+            <div className={classes.recommendationContainer}>
+              <Recommendation key={x.id} {...x} />
+            </div>
           ))}
       </div>
-    </>
+    </div>
   );
 }
 
