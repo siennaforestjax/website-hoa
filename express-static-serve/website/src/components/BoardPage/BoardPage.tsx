109b5f9e48d546a149e3cf52721a2254db2e207b
fl1cker <the_flicker7@hotmail.com>
2022-10-17 18:20:36 -0400

    fixing build

--- express-static-serve/website/src/components/BoardPage/BoardPage.tsx
+++ express-static-serve/website/src/components/BoardPage/BoardPage.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ImageWithLoadingFallback from '../ImageWithLoadingFallback/ImageWithLoadingFallback';
 import classes from './BoardPage.module.css';
 import BoardMember from '../../interfaces/BoardMember';
-import CommitteeMember from '../../interfaces/CommitteeMember';
 import Subcommittee from '../../interfaces/Subcommittee';
+import CommitteeMember from '../../interfaces/CommitteeMember';
 
 export type BoardPageProps = {
   boardMembers: Array<BoardMember>;
   subcommittees: Array<Subcommittee>;
 };
-
 
 const sourceUrl = process.env.REACT_APP_API_URL;
 
@@ -25,17 +24,25 @@ const BoardPage = ({ boardMembers, subcommittees }: BoardPageProps) => {
             key={boardMember.title}
           >
             <figure className={classes.figure}>
-              <ImageWithLoadingFallback source={`${sourceUrl}/${boardMember.imageName}`} altText={`${boardMember.name} (${boardMember.title})`}/>
-              
+              <ImageWithLoadingFallback
+                source={`${sourceUrl}/${boardMember.imageName}`}
+                altText={`${boardMember.name} (${boardMember.title})`}
+              />
+
               <figcaption className={classes.figureCaption}>
                 {boardMember.imageDescription}
               </figcaption>
             </figure>
             <div className={classes.details}>
               <p>
-                <span className={classes.fieldTitle}>{boardMember.title}</span> -<span className={classes.fieldValue}>{boardMember.name}</span>
+                <span className={classes.fieldTitle}>{boardMember.title}</span>{' '}
+                -<span className={classes.fieldValue}>{boardMember.name}</span>
               </p>
-              <p><a href={`mailto: ${boardMember.emailAddress}`}>{boardMember.emailAddress}</a></p>
+              <p>
+                <a href={`mailto: ${boardMember.emailAddress}`}>
+                  {boardMember.emailAddress}
+                </a>
+              </p>
               <p>{boardMember.titleDescription}</p>
             </div>
           </section>
@@ -43,40 +50,43 @@ const BoardPage = ({ boardMembers, subcommittees }: BoardPageProps) => {
       })}
       <h2 className={classes.subcommitteeHeader}>Subcommittees</h2>
       <section className={classes.subcommitteeSection}>
-      {
-        subcommittees.map(sub => {
+        {subcommittees.map((sub: Subcommittee) => {
           return (
-            <section key={sub.name} className={classes.subcommittee + ' colorful-bottom-border'}>
-                <h3 className={classes.committeeName}>{sub.name}</h3>
-              
-                <p>{sub.description}</p>
-                <h3 className={classes.subsection}>Objectives</h3>
-                <ul className={classes.objectiveList}>
-                  {sub.objectives.map(obj => {
-                    return (
-                      <li key={obj} className={classes.objective}>{obj}</li>
-                    )
-                  })}
-                </ul>
-                <div className={classes.subsection}>
-                  <div className={`${classes.row} ${classes.headerRow}`}>
-                    <span className={classes.rowItem}>Member</span>
-                    <span>Role</span>
-                  </div>
-                {
-                  sub.representatives.map(rep => {
-                    return (
+            <section
+              key={sub.name}
+              className={classes.subcommittee + ' colorful-bottom-border'}
+            >
+              <h3 className={classes.committeeName}>{sub.name}</h3>
+
+              <p>{sub.description}</p>
+              <h3 className={classes.subsection}>Objectives</h3>
+              <ul className={classes.objectiveList}>
+                {sub.objectives.map((obj: string) => {
+                  return (
+                    <li key={obj} className={classes.objective}>
+                      {obj}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className={classes.subsection}>
+                <div className={`${classes.row} ${classes.headerRow}`}>
+                  <span className={classes.rowItem}>Member</span>
+                  <span>Role</span>
+                </div>
+                {sub.representatives.map((rep: CommitteeMember) => {
+                  return (
                     <div key={rep.name} className={classes.row}>
                       <span className={classes.rowItem}>{rep.name}</span>
                       <span className={classes.rowItem}>{rep.role}</span>
                     </div>
-                  )})
-                }
-                </div>
-              </section>
-          )})
-        }
-        </section>
+                  );
+                })}
+              </div>
+            </section>
+          );
+        })}
+      </section>
     </>
   );
 };
--- express-static-serve/website/src/components/EventsPage/EventsPage.tsx
+++ express-static-serve/website/src/components/EventsPage/EventsPage.tsx
@@ -9,24 +9,25 @@ type EventsPageProps = {
 function EventsPage({ events }: EventsPageProps) {
   const today = new Date();
   const currYear = today.getFullYear();
-  const currMonth = today.getMonth()+1; // +1 due to zero index
+  const currMonth = today.getMonth() + 1; // +1 due to zero index
 
   console.log(currYear);
 
-  const daysInMonth = new Date(currYear, currMonth, 0).getDate(); 
+  const daysInMonth = new Date(currYear, currMonth, 0).getDate();
 
   return (
     <>
       <h1 className={classes.h1}>Events</h1>
       <div className={classes.calendarContainer}></div>
-      
-        {
-          [...Array(daysInMonth).keys()].map(i => {
-            const numberDate = i+1; // +1 due to zero index
-            const firstDayOfWeek = new Date(currYear, currMonth, 1).getDay();
-            console.log('firstDayOfWeek', firstDayOfWeek)
-          })
-        }
+
+      {[...Array(daysInMonth).keys()].map((i) => {
+        const numberDate = i + 1; // +1 due to zero index
+        const firstDayOfWeek = new Date(currYear, currMonth, 1).getDay();
+        console.log('firstDayOfWeek', firstDayOfWeek);
+
+        // delete this line
+        return <div>{numberDate}</div>;
+      })}
     </>
   );
 }
