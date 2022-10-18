da91b29d0b711f2e630492503b4038303cfd6ee4
fl1cker <the_flicker7@hotmail.com>
2022-10-17 22:31:46 -0400

    new document section

--- express-static-serve/website/src/components/DocumentsPageBasic/DocumentsPage.tsx
+++ express-static-serve/website/src/components/DocumentsPageBasic/DocumentsPage.tsx
@@ -11,44 +11,60 @@ const API_URL = process.env.REACT_APP_API_URL;
 function DocumentsPagePretty() {
   const [docsMinutes, setDocsMinutes] = useState([]);
   const [docsMisc, setDocsMisc] = useState([]);
+  const [docsEssential, setDocsEssential] = useState([]);
   const [isLoadingMinutes, setIsLoadingMinutes] = useState(true);
   const [isLoadingMisc, setIsLoadingMisc] = useState(true);
+  const [isLoadingEssential, setIsLoadingEssential] = useState(true);
   const [minutesDidError, setMinutesDidError] = useState(false);
   const [miscDidError, setMiscDidError] = useState(false);
-  
+  const [essentialDidError, setEssentialDidError] = useState(false);
+
   useEffect(() => {
-    axios.get(`${API_URL}/api/documents/minutes`)
-        .then(response => {
-          setDocsMinutes(response.data);
-          setIsLoadingMinutes(false);
-        })
-        .catch(err => {
-          setIsLoadingMinutes(false);
-          setMinutesDidError(true);
-        });
-  }, [])
+    axios
+      .get(`${API_URL}/api/documents/minutes`)
+      .then((response) => {
+        setDocsMinutes(response.data);
+        setIsLoadingMinutes(false);
+      })
+      .catch((err) => {
+        setIsLoadingMinutes(false);
+        setMinutesDidError(true);
+      });
+  }, []);
   useEffect(() => {
-    axios.get(`${API_URL}/api/documents/misc`)
-        .then(response => {
-          setDocsMisc(response.data);
-          setIsLoadingMisc(false);
-        })
-        .catch(err => {
-          setIsLoadingMisc(false);
-          setMiscDidError(true);
-        });
-  }, [])
+    axios
+      .get(`${API_URL}/api/documents/misc`)
+      .then((response) => {
+        setDocsMisc(response.data);
+        setIsLoadingMisc(false);
+      })
+      .catch((err) => {
+        setIsLoadingMisc(false);
+        setMiscDidError(true);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/documents/essential`)
+      .then((response) => {
+        setDocsEssential(response.data);
+        setIsLoadingEssential(false);
+      })
+      .catch((err) => {
+        setIsLoadingEssential(false);
+        setEssentialDidError(true);
+      });
+  }, []);
 
   function displayDocuments(docs: Array<HoaDocument>) {
     return (
-      
-        <ul className={classes.fileList}>
-          {docs
-            .sort((x, y) => sortStringDates(x.createTime, y.createTime))
-            .map((doc) => (
-              <Document key={doc.filename} document={doc} />
-            ))}
-        </ul>
+      <ul className={classes.fileList}>
+        {docs
+          .sort((x, y) => sortStringDates(x.createTime, y.createTime))
+          .map((doc) => (
+            <Document key={doc.filename} document={doc} />
+          ))}
+      </ul>
     );
   }
 
@@ -56,17 +72,34 @@ function DocumentsPagePretty() {
     <>
       <h1>Documents</h1>
       <div className={classes.fileContainer}>
-        <h2 className={classes.sectionTitle}>HOA Meeting Notes</h2>
-        {
-          isLoadingMinutes ? <LoadingSvg scale={0.8}/> 
-          : minutesDidError ? <p>Error Gathering Documents</p> : displayDocuments(docsMinutes)}
+        <h2 className={classes.sectionTitle}>Homeowner Essentials</h2>
+        {isLoadingMinutes ? (
+          <LoadingSvg scale={0.8} />
+        ) : minutesDidError ? (
+          <p>Error Gathering Documents</p>
+        ) : (
+          displayDocuments(docsEssential)
+        )}
       </div>
       <div className={classes.fileContainer}>
-      <h2 className={classes.sectionTitle}>Miscellaneous Documents</h2>
-      {
-        isLoadingMisc ? <LoadingSvg scale={0.8}/> :
-        miscDidError ? <p>Error Gathering Documents</p> : displayDocuments(docsMisc)
-      }
+        <h2 className={classes.sectionTitle}>HOA Meeting Notes</h2>
+        {isLoadingMinutes ? (
+          <LoadingSvg scale={0.8} />
+        ) : minutesDidError ? (
+          <p>Error Gathering Documents</p>
+        ) : (
+          displayDocuments(docsMinutes)
+        )}
+      </div>
+      <div className={classes.fileContainer}>
+        <h2 className={classes.sectionTitle}>Miscellaneous Documents</h2>
+        {isLoadingMisc ? (
+          <LoadingSvg scale={0.8} />
+        ) : miscDidError ? (
+          <p>Error Gathering Documents</p>
+        ) : (
+          displayDocuments(docsMisc)
+        )}
       </div>
     </>
   );
--- express-static-serve/website/src/components/RecommendationPage/RecommendationPage.tsx
+++ express-static-serve/website/src/components/RecommendationPage/RecommendationPage.tsx
@@ -4,18 +4,17 @@ import './RecommendationPage.module.css';
 import classes from './RecommendationPage.module.css';
 import mock from '../../recommendations.json';
 
-
 function RecommendationPage() {
-  
   return (
     <>
       <h1 className={classes.pageTitle}>Recommendations</h1>
-      <div className={classes.recommendationListContainer}>
       <p className={classes.pageDescription}>
-        Looking to have some work done around the house?  Check out these recommendations from your neighbors.
+        Looking to have some work done around the house? Check out these
+        recommendations from your neighbors.
       </p>
+      <div className={classes.recommendationListContainer}>
         {mock.recommendations
-          .filter((x) => (x.category === 'contractor'))
+          .filter((x) => x.category === 'contractor')
           .map((x) => (
             <div key={x.id} className={classes.recommendationContainer}>
               <Recommendation {...x} />
