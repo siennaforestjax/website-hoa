fb9585f26be4459af1f2eedf7663b0243bf69b9a
fl1cker <the_flicker7@hotmail.com>
2022-11-03 16:07:50 -0400

    removing unneeded vars

--- express-static-serve/website/src/components/DocumentsPageBasic/DocumentsPage.tsx
+++ express-static-serve/website/src/components/DocumentsPageBasic/DocumentsPage.tsx
@@ -14,10 +14,8 @@ function DocumentsPagePretty() {
   const [docsEssential, setDocsEssential] = useState([]);
   const [isLoadingMinutes, setIsLoadingMinutes] = useState(true);
   const [isLoadingMisc, setIsLoadingMisc] = useState(true);
-  const [isLoadingEssential, setIsLoadingEssential] = useState(true);
   const [minutesDidError, setMinutesDidError] = useState(false);
   const [miscDidError, setMiscDidError] = useState(false);
-  const [essentialDidError, setEssentialDidError] = useState(false);
 
   useEffect(() => {
     axios
