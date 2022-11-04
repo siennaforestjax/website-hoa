e29340606401534c0d5d7cfd6214b228582bcabd
fl1cker <the_flicker7@hotmail.com>
2022-11-04 12:28:27 -0400

    testing

--- express-static-serve/website/src/components/ImageWithLoadingFallback/ImageWithLoadingFallback.tsx
+++ express-static-serve/website/src/components/ImageWithLoadingFallback/ImageWithLoadingFallback.tsx
@@ -1,35 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import LoadingSvg from '../LoadingSvg/LoadingSvg';
 import classes from './ImageWithLoadingFallback.module.css';
 
 type ImageWithLoadingFallbackProps = {
-    source: string;
-    altText: string;
-    loadingScale?: number;
-}
+  source: string;
+  altText: string;
+  loadingScale?: number;
+};
 
-function ImageWithLoadingFallback({source, altText, loadingScale = 1, ...props} : ImageWithLoadingFallbackProps) {
+function ImageWithLoadingFallback({
+  source,
+  altText,
+  loadingScale = 1,
+  ...props
+}: ImageWithLoadingFallbackProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
+  function onLoadHandler() {
+    setIsLoading(false);
+  }
 
-    function onLoadHandler() {
-        setIsLoading(false);
-    }
+  function onErrorHandler() {
+    setIsLoading(false);
+    setIsError(true);
+  }
 
-    function onErrorHandler() {
-        setIsLoading(false);
-        setIsError(true);
-    }
-
-    return (
-        <div className={`${classes.imageWrapper} ${isLoading ? classes.loading : isError ? classes.error : ''}`}> 
-        {
-            isLoading ? <LoadingSvg scale={loadingScale}/> : isError ? <p className={classes.errorMessage}>Image Failed To Load</p> : ''
-        }
-            <img className={classes.mainImage}src={source} alt={altText} onLoad={onLoadHandler} onError={onErrorHandler} {...props}/>
-        </div>
-    );
+  return (
+    <div
+      className={`${classes.imageWrapper} ${
+        isLoading ? classes.loading : isError ? classes.error : ''
+      }`}
+    >
+      {isLoading ? (
+        <LoadingSvg scale={loadingScale} />
+      ) : isError ? (
+        <p className={classes.errorMessage}>Image Failed To Load</p>
+      ) : (
+        ''
+      )}
+      <img
+        className={classes.mainImage}
+        src={source}
+        alt={altText}
+        onLoad={onLoadHandler}
+        onError={onErrorHandler}
+        {...props}
+      />
+    </div>
+  );
 }
 
 export default ImageWithLoadingFallback;
